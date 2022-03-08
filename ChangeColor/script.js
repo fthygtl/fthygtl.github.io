62ff97ed090cc875adb819a206838d88dcd60485
Fatih Yiğitel <70219430+fthygtl@users.noreply.github.com>
2022-03-08 18:30:20 +0300

    Update script.js

--- ChangeColor/script.js
+++ ChangeColor/script.js
@@ -1,7 +1,7 @@
 const colorBtn = document.querySelector('.colorBtn');
 const bodyBcg = document.querySelector('body');
 
-const colors = ['yellow','red','green','#3b5998','pink'];
+const colors = ['yellow','red','green','#3b5998','pink', '#733b98'];
 
 
 colorBtn.addEventListener('click', changeColor);
